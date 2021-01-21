34331fe320166e28d74d2619310a0e1f9ca8c83e
Sadid <76834719+Sadid-USN@users.noreply.github.com>
2021-01-21 20:48:44 +0300

    Update Class Person with attributes Class.py

--- Python class/Class Person with attributes Class.py
+++ Python class/Class Person with attributes Class.py
@@ -7,20 +7,23 @@ class Person:
         self.yearOfBirth = yearOfBirth
         Person.people_count += 1
 
-    def information(self, n):
-        for i in range(n):
-            print(f"Name: {self.name}, Surname: {self.surname}, PlactOfBirth: {self.placeOfBirth}, yearOfBirth {self.yearOfBirth}")
-    def getAge(self, currentYear):
-                print(f"Age: {currentYear - self.yearOfBirth}")
+    def information(self):
+        print(f"Name: {self.name}, Surname: {self.surname}, PlactOfBirth: {self.placeOfBirth}, yearOfBirth {self.yearOfBirth}")
+
+    def getAge(self,):
+        currentYear = 2021
+        print(f"Age:  {self.name} = {currentYear - self.yearOfBirth}")
 
 
 p1 = Person("Sadid", "Idibekov", "Tajikistan", 1988)
-p2 = Person("Kobe", "Bryant", "USA", 1978)
+p2 = Person("Michael ", "Jordan", "USA", 1963)
 p3 = Person("Ilon", "Musk", "USA", 1971)
-p1.test = "hello"
 
 
-p1.information(1)
-p2.information(1)
-p3.information(1)
-p3.getAge(2021)
+
+p1.information()
+p2.information()
+p3.information()
+p1.getAge()
+p2.getAge()
+p3.getAge()
